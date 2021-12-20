bc7abbf099aa8055c170927805852af57fb6efe7
nm125 <nm125@hw.ac.uk>
2021-12-20 23:27:56 +0000

    hashMap implemented, quite laggy still

--- Move.js
+++ Move.js
@@ -1,7 +1,8 @@
-import {Board, BOARD, canvas, pieces} from "./board.js";
-import {bishop, Piece, wP} from "./Piece.js";
-import {Tile} from "./Tile.js";
-import {Game} from "../Game.js";
+import {Board, BOARD, canvas, pieces, table} from "./modules/board.js";
+import {bishop, Piece, wP} from "./modules/Piece.js";
+import {Tile} from "./modules/Tile.js";
+// import {Game} from "./Game.js";
+
 
 
 
@@ -16,12 +17,6 @@ class Move {
     // Moves a piece on the canvas by listening to 3 mouse events: mousedown, mouseup and mousemove
     movePieceUI() {
         
-        canvas.onmousedown = myDown;
-        canvas.onmouseup = myUp;
-        canvas.onmousemove = myMove;
-
-
-
         // const canvas = document.getElementById("canvas");
         const ctx = canvas.getContext("2d");
 
@@ -48,10 +43,12 @@ class Move {
         function draw(newX, newY) {
             // Draw the board
             self.board.drawBoard();
-
+            // Get all the values from the hashmap
+            let values = table.values();
             // redraw each piece in the pieces[] array
-            for (let i = 0; i < pieces.length; i++) {
-                let piece = pieces[i];
+            for (let piece of values) {
+                // If we are dealing with 0's or undefined then move to the next value in the map
+                if (piece === undefined) continue;
                 // if the piece has an image and has NOT moved coords then draw it onto the canvas with the same coords
                 if (piece.x != newX && piece.image || piece.y != newY && piece.image) {
                     ctx.drawImage(piece.image, piece.x, piece.y);
@@ -74,10 +71,11 @@ class Move {
             let mx = parseInt(e.clientX - offsetX);
             let my = parseInt(e.clientY - offsetY);
 
+            let values = table.values();
             // test each rect to see if mouse is inside
             dragok = false;
-            for (let i = 0; i < pieces.length; i++) {
-                let piece = pieces[i];
+            for (let piece of values) {
+                if (piece === undefined) continue;
                 // If the mouse is inside the piece when we click then set dragging properties to be true
                 if (mx > piece.x && mx < piece.x + piece.width && my > piece.y && my < piece.y + piece.height) {
                     // if yes, set that pieces isDragging=true
@@ -88,7 +86,7 @@ class Move {
                     let pieceType = piece._type;
                     let pieceColour = piece._isWhite;
                     console.log("I am a: ", pieceColour, " ", pieceType);
-
+                    // If the piece is a pawn then draw valid moves
                     switch(pieceType) {
                         case "pawn":
                             if (pieceColour) {
@@ -112,28 +110,31 @@ class Move {
             e.preventDefault();
             e.stopPropagation();
 
-            this.game = new Game();
+            // this.game = new Game();
 
             let closest;
+            let values = table.values();
 
             // clear all the dragging flags
             dragok = false;
-            for (let i = 0; i < pieces.length; i++) {
-                if (pieces[i].isDragging) {
-                    pieces[i].isDragging = false;
+            for (let piece of values) {
+                if (piece === undefined) break;
+
+                if (piece.isDragging) {
+                    piece.isDragging = false;
 
                     // Calls to the method getClosestTile which will find the closest tile for the given piece
-                    closest = self.getClosestTile(pieces[i]);
+                    closest = self.getClosestTile(piece);
                     
                     // Saves the x and y for the piece we have moved
-                    const oldX = pieces[i].x;
-                    const oldY = pieces[i].y;
+                    const oldX = piece.x;
+                    const oldY = piece.y;
                     // Sets the new x and y for the piece we have moved
-                    pieces[i].x = closest.screenX;
-                    pieces[i].y = closest.screenY;
+                    piece.x = closest.screenX;
+                    piece.y = closest.screenY;
                     // If the piece has moved then draw the piece with the new x and y coords
-                    if (pieces[i].x != oldX || pieces[i].y != oldY) {
-                        draw(pieces[i].x, pieces[i].y);
+                    if (piece.x != oldX || piece.y != oldY) {
+                        draw(piece.x, piece.y);
                         self.playMoveSound();   
 
                         let target;
@@ -141,26 +142,14 @@ class Move {
                             target = f.filter(t => {return t.screenX == closest.screenX && t.screenY == closest.screenY})
                             // console.log("This is the target, ", target);
                         }
-                        this.game.playMove(pieces[i], target);
+                        // self.playMove(piece, target);
 
-
-
-
-
-
-                        // // Loop through each element of our boards FILE and RANK 
-                        // for (let file of BOARD) {
-                        //     for (let rank of file) {
-                        //         // If the current tile is the same as the tile we have placed our piece on then set our target piece to be the current tile
-                        //         if (rank.screenX == closest.screenX && rank.screenY == closest.screenY) {
-                        //             this.game.playMove(pieces[i], rank);
-                        //             // console.log("this is our target tile: ", target);
-                        //         }
-                        //     }
-                        // }
-                        
+                        // Collision for taking pieces
+                        if (!(piece.tile.isEmpty)) {
+                            // Remove the piece that is on the board and keep the piece we have just placed
+                            
+                        }
                     }
-                    // console.log("This is the end X coord: ", pieces[i].x, " and the end Y coord: ", pieces[i].y);
                 }
             }
         }
@@ -185,24 +174,25 @@ class Move {
                 let dy = my - startY;
 
                 let pieceMoves;
+                let values = table.values();
 
                 // move each piece that isDragging is true for
                 // by the distance the mouse has moved
                 // since the last mousemove
-                for (let i = 0; i < pieces.length; i++) {
-                    let piece = pieces[i];
+                for (let piece of values) {
+                    if (piece === undefined) continue;
+
                     if (piece.isDragging) {
                         piece.x += dx;
                         piece.y += dy;
 
                         pieceMoves = piece;
-
-
                     }
                 }
                 // redraw the scene with any updates to the piece positions
                 draw();
 // TODO: Think of a better way to do this
+                // Draws the valid moves for the piece we are moving
                 self.drawValid(pieceMoves);
 
                 // reset the starting mouse position for the next mousemove
@@ -220,23 +210,14 @@ class Move {
  * @returns the tile that is closest to the piece we are moving
  */
     getClosestTile(piece) {
+        let tiles = table.keys();
 
-                
+        for (let tile of tiles) {
+            let difX = Math.abs(piece.x - tile.screenX);
+            let difY = Math.abs(piece.y - tile.screenY);
 
-        for (let i = 0; i < BOARD.length; i++) {
-            for (let j = 0; j < BOARD[0].length; j++) {
-
-                let diffX = Math.abs(piece.x - BOARD[i][j].screenX);
-                let diffY = Math.abs(piece.y - BOARD[i][j].screenY);
-
-                // console.log("newDiffX = ", newDiffX);
-                // console.log("current tile = ", BOARD[i][j]);
-
-                if (diffX <= 50 && diffY <= 50) {
-                    // console.log("We need to be on FILE: ", BOARD[i][j].x);
-                    // console.log("We need to be on RANK: ", BOARD[i][j].y);
-                    return BOARD[i][j];
-                }
+            if (difX <= 50 && difY <= 50) {
+                return tile;
             }
         }
     }
@@ -253,10 +234,10 @@ class Move {
         const ctx = canvas.getContext("2d");
 
 
-        this.game = new Game();
+        // this.game = new Game();
 
         let validMoves = new Set();
-        validMoves = this.game.getPossibleMoves(piece);
+        validMoves = this.getPossibleMoves(piece);
         // console.log("valid moves: ", validMoves);
         for (let move of validMoves) {
             // ctx.fillStyle = "red";
@@ -270,6 +251,88 @@ class Move {
         
     }
 
+    /**
+    Pawns can ONLY move +- 1 on the Y axis
+    Rooks can move +- 7 on X and Y axis
+
+     Gets all the possible moves for a given piece
+     TODO: Finish implementing this -> currently only hard coded the pawns first 2 moves
+
+
+    @param {Piece} piece a given piece on the chess board that we will return all the possible moves for
+    @returns  a set of all possible moves for a given piece
+
+    **/ 
+    // Possibly move this to Move.js
+    getPossibleMoves(piece) {
+
+        let moveSet = new Set();
+
+        if (piece.type == "pawn") {
+
+            // moveSet.add(pawnRules(piece));
+
+
+
+    //             let tile = piece.tile;
+    //             // console.log("current tile: ", tile);
+    //             // Using _y here to ensure that we dont change the property as there is no setter it will give an error if the property is changed
+    //             if (piece.tile._y == 2) {
+
+    // // TODO: Look at how to make this dynamic and not hard coded with the + 1 and + 2 -> probably going to need maths to find a formula for the moves for each piece 
+    //                 for (let i = 0; i < BOARD.length; i++) {
+    //                     for (let j = 0; j < BOARD[0].length; j++) {
+    //                         if (BOARD[i][j] == tile) {
+    //                             moveSet.add(BOARD[i][j + 1]);
+    //                             moveSet.add(BOARD[i][j + 2]);
+    //                             // console.log("We just added moves: ", moveSet, " for piece: ", piece.p);
+    //                         }
+    //                     }
+    //                 }
+    //             }
+        }
+        return moveSet;
+    }
+
+    /** 
+     * Plays a move in our array board and updates all the associated values "behind the scenes".
+     * 
+     * 
+     * 
+     * @param {Piece} piece the piece we are moving from one tile to another 
+     * @param {Tile} target the target tile we are moving our piece to
+    */
+    //  playMove(piece, target) {
+    //     let startTile = piece.getTile;
+
+        
+
+    //     console.log("Start tile before any changes: ", piece.tile);
+
+    //     piece._isEmpty = true;; // Set the tile we have moved from to be true
+
+    //     target._isEmpty = false; // Setting the target tile to having a piece on it
+
+    //     piece._tile = target; // Update our piece to be on our new tile
+
+    //      console.log("Target tile after changes: ", target);
+    //      console.log("Start tile after changes: ", piece.tile);
+    //      console.log("This is our piece we have moved: ", piece);
+    //      console.log("to tile: ", piece.tile);
+
+    // }
+
+
+    run() {
+        // this.board.initBoard();
+        // this.board.populate();
+        // this.movePieceUI();
+        this.board.createBoardHash();
+        this.board.populateHash();
+        this.movePieceUI();
+    }
+
+
 
     playMoveSound() {
         let audio = new Audio("../assets/move.mp3");
@@ -282,6 +345,10 @@ class Move {
     }
 
 }
+
+const start = new Move();
+
+start.run();
 
 
 
